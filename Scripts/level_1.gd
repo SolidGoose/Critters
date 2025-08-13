6b1dc53b2c27ccbc7b3eee94f224018e68d067eb
kirthivaasan <kirthivaasanp@gmail.com>
2025-08-13 21:15:18 +0300

    add text processing logic

--- Scripts/level_1.gd
+++ Scripts/level_1.gd
@@ -46,11 +46,73 @@ func create_blast_area(spawnPosition: Vector2) -> void:
 	blast.global_position = spawnPosition
 	add_child(blast)
 
+# A bunch of functions for matching string patterns
+func isPattern(text:String, pattern: String) -> String:
+	var regex = RegEx.new()
+	regex.compile(pattern)
+	var result = regex.search(text)
+	if result:
+		if result.get_string().length() == text.length():
+			return result.get_string()
+		else:
+			return ""
+	else:
+		return ""
+	
+func isBracketOrSignPattern(text: String) -> String:
+	var pattern = r"\[(\w+)\](\?|\!)?"
+	return isPattern(text, pattern)
+	
+func isWordOrSignPattern(text: String) -> String:
+	var pattern = r"(\w+)(\?|\!)?"
+	return isPattern(text, pattern)
+	
+func isLastCharSign(text: String) -> String:
+	var c = text[text.length()-1]
+	if c == "!" or c == "?": # add more signs here if necessary
+		return c
+	else:
+		return ""
+	
+func getBracketCmd(text:String) -> String:
+	var regex = RegEx.new()
+	var pattern =  r"\[(\w+)\]"
+	regex.compile(pattern)
+	var matches = regex.search_all(text)
+	if matches.size() > 0:
+		return matches[0].get_string(1)
+	else:
+		return ""
+
+func isSentencePattern(text: String) -> String:
+	var pattern = r"^\w+( \w+)*"
+	return isPattern(text, pattern)
 
 func _on_console_line_text_submitted(new_text: String) -> void:
 	var enemyNodes: Array[Node] = get_tree().get_nodes_in_group('EnemyPathFollow')
 	enemyNodes.sort_custom(func (a, b): return a.progress_ratio > b.progress_ratio)
-	print(enemyNodes)
+	#print(enemyNodes)
+	
+	# Text processing logic. In the end the command is stored in cmd, and sign in sign.
+	# Sentence is stored if found in sentence_str
+	var bracketsign_str = isBracketOrSignPattern(new_text)
+	var wordsign_str = isWordOrSignPattern(new_text)
+	var sentence_str = isSentencePattern(new_text)
+	var cmd = ""
+	var sign = ""
+	
+	if bracketsign_str != "":
+		cmd = getBracketCmd(bracketsign_str)
+		sign = isLastCharSign(bracketsign_str)
+		
+	if wordsign_str != "":
+		sign = isLastCharSign(wordsign_str)
+		if sign == "":
+			cmd = wordsign_str
+		else:
+			cmd = wordsign_str.substr(0, wordsign_str.length()-1)
+		
+	print("cmd: " + cmd + ", sign: " + sign)		
 
 	if enemyNodes.size() > 0:
 		var wordsDict = enemyNodes.map(get_word_dict_from_node)
